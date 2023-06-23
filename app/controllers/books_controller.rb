5b1d1421a22d39c9e522f05b3561450763ee5aa3
EC2 Default User <132799867+momopomi@users.noreply.github.com>
2023-06-23 14:46:26 +0000

    [edit]3-15

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+
+  end
+
+  def create
+    # １. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿が成功しました"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "1e11ff2c52c046efb13acd56226ff653.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
